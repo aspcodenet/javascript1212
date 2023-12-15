c8098f931127671634aaa0b07e958d844c2cc2e8
Stefan Holmberg <stefan.holmberg@systementor.se>
2023-12-15 10:41:58 +0100

    aa

---
+++ christmas/ChristmasList.js
@@ -0,0 +1,9 @@
+
+// PUBLIC, PRIVATE
+
+export function importantList ({iWant, iDontWant, iNeed}) {
+	return `${iWant} is what i want!  Dont give me: ${iDontWant} . Sadly I need  ${iNeed}`;
+}
+
+
+export const theDay = 24
